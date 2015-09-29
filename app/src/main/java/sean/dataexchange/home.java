f1b1bc611445eba8e9dfa837568555fa783e0235
seanbader <sbader@mymail.mines.edu>
2015-09-28 18:55:01 -0600

    Created bluetooth settings - can now link devices through app This is untested Home page created

--- app/src/main/java/sean/dataexchange/home.java
+++ app/src/main/java/sean/dataexchange/home.java
@@ -1,41 +1,40 @@
 package sean.dataexchange;
 
+/**
+ * Created by Sean on 9/28/2015.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Set;
 
-public class DataTransfer extends AppCompatActivity {
-
+public class home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.data_transfer);
+        setContentView(R.layout.home_screen);
 
-        Button bt_settings;
+        /**
+         * Link to Bluetooth Settings
+         */
+        Button settings;
         final Context context = this;
-        bt_settings = (Button) findViewById(R.id.bt_settings);
+        settings = (Button) findViewById(R.id.settings);
 
-
-        bt_settings.setOnClickListener(new OnClickListener() {
+        settings.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                Intent intent = new Intent(context, MainActivity.class);
+                Intent intent = new Intent(context, bt_settings.class);
                 startActivity(intent);
             }
         });
